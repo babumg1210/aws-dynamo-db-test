b969b33bc9bd9659f6d4dcc17d8aa257e705bdc5
babumg1210 <babumg1210@outlook.com>
2025-02-17 21:19:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,49 @@
-resource "aws_dynamodb_table" "notes_table" {
-  name           = "notes-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
+# Define the DynamoDB table
+resource "aws_dynamodb_table" "tf_notes_table" {
+  name             = "tf-notes-table"
+  billing_mode     = "PROVISIONED"
+  read_capacity    = 30
+  write_capacity   = 30
 
   attribute {
     name = "noteId"
-    type = "S"  # String type
+    type = "S"
   }
 
   hash_key = "noteId"
 
   ttl {
-    enabled        = true
-    attribute_name = "expiryPeriod"
+    enabled         = true
+    attribute_name  = "expiryPeriod"
   }
 
   point_in_time_recovery {
-    enabled = true  # Enabling point-in-time recovery (PITR)
+    enabled = true
   }
 
   server_side_encryption {
-    enabled = true  # Enabling encryption at rest
+    enabled = true
+  }
+
+  lifecycle {
+    ignore_changes = [ "write_capacity", "read_capacity" ]
   }
 }
 
+# Define the auto-scaling for read capacity
 resource "aws_appautoscaling_target" "read_capacity" {
-  max_capacity       = 100
-  min_capacity       = 5
+  max_capacity       = 50
+  min_capacity       = 10
   resource_id        = "table/${aws_dynamodb_table.tf_notes_table.name}"
   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
   service_namespace  = "dynamodb"
 }
 
+# Define the auto-scaling for write capacity
 resource "aws_appautoscaling_target" "write_capacity" {
-  max_capacity       = 100
-  min_capacity       = 5
+  max_capacity       = 50
+  min_capacity       = 10
   resource_id        = "table/${aws_dynamodb_table.tf_notes_table.name}"
   scalable_dimension = "dynamodb:table:WriteCapacityUnits"
   service_namespace  = "dynamodb"
 }
-
