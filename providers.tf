5dd92b75f129a65265d7dd7c77e6312c70e16be2
babumg1210 <babumg1210@outlook.com>
2025-02-17 21:16:07 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "us-east-1"  # Make sure to update this as per your requirement
+
+  # No need to specify version here, as the version is already defined in required_providers block.
+}
